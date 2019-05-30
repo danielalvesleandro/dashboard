97e207b0560a2d971370229b8f418a607cd2d63e
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-05-29 22:11:21 -0300

    dashboard

---
+++ blueprints/gitlab.py
@@ -0,0 +1,15 @@
+
+
+import flask
+
+blueprint = flask.Blueprint('gitlab', __name__)
+
+@blueprint.route('/gitlab', methods=[ 'GET' ])
+def get_gitlab():
+
+    context = {
+        'page': 'gitlab'
+    }
+
+    return flask.render_template('gitlab.html', context=context)
+
